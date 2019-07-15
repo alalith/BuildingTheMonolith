0ac996af8ab6fbe703e96b2c907df9c1be99cc7a
alalith <arjunlalith@gmail.com>
2019-07-15 14:21:09 -0500

    Added Week 6. I now need to add the ability to store training maxes and calculate weight based off of that training max.

---
+++ app/src/main/java/com/arjunlalith/buildingthemonolith/Week6WednesdayFragment.kt
@@ -0,0 +1,18 @@
+package com.arjunlalith.buildingthemonolith
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Week6WednesdayFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_week6_wednesday, container, false)
+    }
+}
