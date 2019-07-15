aee96325600f528bbbdeea92e7275f6495b90af5
alalith <arjunlalith@gmail.com>
2019-07-15 12:06:25 -0500

    Added Week 5.

---
+++ app/src/main/java/com/arjunlalith/buildingthemonolith/Week5Fragment.kt
@@ -0,0 +1,57 @@
+package com.arjunlalith.buildingthemonolith
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class Week5Fragment : Fragment() {
+    private lateinit var daysPagerAdapter: DaysPagerAdapter
+    private lateinit var viewPager: ViewPager
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_week5, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        daysPagerAdapter = DaysPagerAdapter(childFragmentManager)
+        viewPager = view.findViewById(R.id.days_pager)
+        viewPager.adapter = daysPagerAdapter
+        val tabs: TabLayout = view.findViewById(R.id.tabLayout)
+        tabs.setupWithViewPager(viewPager)
+    }
+    inner class DaysPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment? {
+            when(position) {
+                0 -> {
+                    return Week5MondayFragment()
+                }
+                1 -> {
+                    return Week5WednesdayFragment()
+                }
+                2 -> {
+                    return Week5FridayFragment()
+                }
+                else -> return null
+            }
+        }
+        override fun getCount(): Int {
+            return 3
+        }
+        override fun getPageTitle(position: Int): CharSequence? {
+           when(position) {
+               0-> return "Monday"
+               1-> return "Wednesday"
+               2-> return "Friday"
+           }
+           return null
+        }
+    }
+}
